4f7097d9afb4000132bfd44f3bae21f2112fb43c
Sao <Vipin.sao@ssipmt.com>
2025-05-24 12:03:20 +0530

    Created a Javascript list

---
+++ JavaScript/Lecture1/basic.js
@@ -0,0 +1,8 @@
+//Javascript is just a programming language for web applications
+// It is used to make web pages interactive and dynamic
+
+//we can link javascript in two ways
+//1. Internal Linking
+//2. External Linking
+
+console.log("Hello this is basic");
